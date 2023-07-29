8fc48c70eeefa93c21cd0e2e78e8c5aa54ad73b0
Kamal Kumar <iamkamalkumar17@gmail.com>
2023-07-29 13:17:31 +0530

    working copy till finish

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,6 +19,19 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+app.get("/post/:postName", (req,res)=>{
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if(storedTitle === requestedTitle) {
+      res.render("post.ejs", {content: post})
+    }
+  })
+  // res.redirect("/");
+})
 
 app.post("/submit", (req, res) => {
   const post = {
